f3cb145e60c73d645e1c8c3abebdae3e64bc8369
Lauri Korts-Pärn <lauri@korc.jp>
2017-01-13 14:43:25 -0500

    reorganize compile sequence / tools

--- usr/src/sfs.d/.common.sh
+++ usr/src/sfs.d/.common.sh
@@ -2,3 +2,15 @@
 . "$lbu/scripts/common.func"
 
 test -n "${SILENT_EXIT+set}" || SILENT_EXIT=1
+
+case ":$PATH:" in
+  *:$HOME/.local/bin:*) ;;
+  *) export PATH="$HOME/.local/bin:$PATH" ;;
+esac
+
+pip_inst() {
+  CPPFLAGS="-I/opt/include" LDFLAGS="-Wl,-rpath=/opt/lib -L/opt/lib" pip install \
+    --upgrade --install-option="--prefix=/opt" --root "$DESTDIR" \
+    ${pip_switches} \
+    "$@"
+}
